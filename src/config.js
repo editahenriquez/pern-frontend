508f97f84812457cdac61c94c851947418d9b10e
Edita <edita.henriquezv@gmail.com>
2023-12-02 17:37:37 -0500

    build updated

--- src/config.js
+++ src/config.js
@@ -1,2 +1,4 @@
 const BASE_URL = process.env.REACT_APP_BASE_URL || 'http://localhost:4000';
+alert(`env.REACT_APP_BASE_URL:${process.env.REACT_APP_BASE_URL}`);
+alert(`BASE_URL:${BASE_URL}`);
 export default BASE_URL;
